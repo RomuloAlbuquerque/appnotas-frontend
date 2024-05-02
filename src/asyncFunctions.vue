c463c67994c1a542d96d6b8c9312534ec0d90772
RomuloAlbuquerque <66214784+RomuloAlbuquerque@users.noreply.github.com>
2024-05-02 02:29:29 -0300

    mudança de url para urlRailway em asyncFunctions.vue

--- src/asyncFunctions.vue
+++ src/asyncFunctions.vue
@@ -3,28 +3,28 @@ import axios from 'axios';
 import { computed, ref } from 'vue';
 
 const urlReplit = 'https://27499262-9023-401e-aa24-9c07eae665d0-00-1tjf1vaq9hopo.worf.replit.dev/'
-
+const urlRailway = 'https://appnotas-backend-production.up.railway.app/'
 
 export const lista = ref([])
 
 export const atualizarInstanciaDeLista = async () =>
-    await axios.get(`${urlReplit}readAllNotes`)
+    await axios.get(`${urlRailway}readAllNotes`)
         .then((res) => lista.value = res.data).catch((error) => console.log(error))
 
 export const adicionarNota = async (obj) => {
-    await axios.post(`${urlReplit}create/`, obj)
+    await axios.post(`${urlRailway}create/`, obj)
         .catch((error) => console.log(error))
     atualizarInstanciaDeLista()
 }
 
 export const atualizarNota = async (id, obj) => {
-    await axios.put(`${urlReplit}update/${id}`, obj)
+    await axios.put(`${urlRailway}update/${id}`, obj)
         .catch((error) => console.log(error))
     atualizarInstanciaDeLista()
 }
 
 export const deletarNota = async (id) => {
-    await axios.delete(`${urlReplit}remove/${id}`).then((res) => console.log(res.data))
+    await axios.delete(`${urlRailway}remove/${id}`).then((res) => console.log(res.data))
         .catch((error) => console.log(error))
     atualizarInstanciaDeLista()
 }
