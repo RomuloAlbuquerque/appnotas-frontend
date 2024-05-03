23fa9387e1a5cc0f9d54d3f429b83e70fca8c4bc
RomuloAlbuquerque <66214784+RomuloAlbuquerque@users.noreply.github.com>
2024-05-03 15:03:47 -0300

    correcao bug login

--- src/asyncFunctions.vue
+++ src/asyncFunctions.vue
@@ -3,38 +3,41 @@ import axios from 'axios';
 import { ref } from 'vue';
 
 const local = 'http://localhost:3001/'
-const urlReplit = 'https://27499262-9023-401e-aa24-9c07eae665d0-00-1tjf1vaq9hopo.worf.replit.dev/'
-const urlRailway = 'https://appnotas-backend-production.up.railway.app/'
+const replit = 'https://27499262-9023-401e-aa24-9c07eae665d0-00-1tjf1vaq9hopo.worf.replit.dev/'
+const railway = 'https://appnotas-backend-production.up.railway.app/'
+const servidorAtual = ref(railway)
+
+console.log('servidor atual ',servidorAtual.value)
 
 export const lista = ref([])
 
 export const atualizarInstanciaDeLista = async (iduser) => {
-    await axios.get(`${urlRailway}readAllNotes/${iduser}`)
+    await axios.get(`${servidorAtual.value}readAllNotes/${iduser}`)
         .then((res) => lista.value = res.data).catch((error) => console.log(error))
 }
 
 export const adicionarNota = async (obj) => {
-    await axios.post(`${urlRailway}create/`, obj)
+    await axios.post(`${servidorAtual.value}create/`, obj)
         .catch((error) => console.log(error))
     atualizarInstanciaDeLista(obj.iduser)
 }
 export const atualizarNota = async (idnota, obj) => {
-    await axios.put(`${urlRailway}update/${idnota}`, obj)
+    await axios.put(`${servidorAtual.value}update/${idnota}`, obj)
         .catch((error) => console.log(error))
     atualizarInstanciaDeLista(obj.iduser)
 }
 export const deletarNota = async (idnota, iduser) => {
-    await axios.delete(`${urlRailway}remove/${idnota}`).then((res) => console.log(res.data))
+    await axios.delete(`${servidorAtual.value}remove/${idnota}`).then((res) => console.log(res.data))
         .catch((error) => console.log(error))
     atualizarInstanciaDeLista(iduser)
 }
 //usuarios
 export const cadastrarUsuario = async (obj) => {
-    await axios.post(`${urlRailway}createUser/`, obj)
+    await axios.post(`${servidorAtual.value}createUser/`, obj)
         .catch((error) => console.log(error))
 }
 export const verificarLogin = async (obj) =>
-    await axios.post(`${urlRailway}checkLogin/`, obj).then((res) => res.data)
+    await axios.post(`${servidorAtual.value}checkLogin/`, obj).then((res) => res.data)
         .catch((error) => console.log(error))
 
 </script>
