7f720ea0682789e82cde97a97a06d4b9c557ec76
RomuloAlbuquerque <66214784+RomuloAlbuquerque@users.noreply.github.com>
2024-05-11 18:36:55 -0300

    set url de local para railway

--- src/asyncFunctions.vue
+++ src/asyncFunctions.vue
@@ -6,7 +6,7 @@ import security from './services/security';
 const local = 'http://localhost:3001/'
 const replit = 'https://27499262-9023-401e-aa24-9c07eae665d0-00-1tjf1vaq9hopo.worf.replit.dev/'
 const railway = 'https://appnotas-backend-production.up.railway.app/'
-const servidorAtual = ref(local)
+const servidorAtual = ref(railway)
 
 export const lista = ref([])
 
